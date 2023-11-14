404c5eb0a95c6bcd1d310aa24d0507b7d9986fa2
CamilaBorges04 <camilaescola998@gmail.com>
2023-11-14 17:35:52 +0000

    save01

---
+++ aula01use/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './App.css';
+import CalculadoraDeSoma from './components/soma';
+import CalculadoraDeSubtracao from './components/subtracao';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1>Minha primeira calculadora em React</h1>
+      <h2>Calcular Soma</h2>
+      <CalculadoraDeSoma/>
+      <h2>Calcular Subtração</h2>
+      <CalculadoraDeSubtracao/>
+    </div>
+  );
+}
+
+export default App;
---
+++ aula01use/src/components/subtracao.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+function CalculadoraDeSubtracao() {
+    const [numero1, setNumero1] = useState('')
+    const [numero2, setNumero2] = useState('')
+    const [subtracao, setSubtracao] = useState(null);
+
+    const calcularSubtracao = () => {
+        const resultado = Number(numero1) - Number(numero2);
+        setSubtracao(resultado);
+    }
+
+    return(
+        <div className='Calculadora'>
+            <input 
+            type='number' 
+            value={numero1} 
+            onChange={(e) => setNumero1(e.target.value)} 
+            placeholder='Digite o primeiro número:'
+            />
+            
+            <input 
+            type='number' 
+            value={numero2} 
+            onChange={(e) => setNumero2(e.target.value)} 
+            placeholder='Digite o segundo número:'
+            />
+            
+            <button onClick={calcularSubtracao}>Calcular Subtração</button>
+            {subtracao !== null && <p>Resultado: {subtracao}</p>}
+        </div>
+    )
+
+}
+export default CalculadoraDeSubtracao
